505db604ec3f2f8cc2a18a0ae8579b675a2976f1
Justice A. Acheampong <just.appiah@gmail.com>
2022-03-16 10:28:08 -0400

    edit page working

--- products/views/Edit.jsx
+++ products/views/Edit.jsx
@@ -4,13 +4,8 @@ class Edit extends React.Component{
     render() {
       return (
   
-        <html lang="en">
-        <head>
-         <meta charset="UTF-8" />
-       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-         <title></title>
-        </head>
+       
+       
         <body  style={{backgroundImage: 'url()'}} >
 
         <link rel="stylesheet" href="/css/app.css"/> 
@@ -23,7 +18,7 @@ class Edit extends React.Component{
         
        {/* See the Layout takes in a prop called Title and we pass Edit Page to it  note: comments can't go first or last in  jsx return*/}
             {/* form is not complete we will do that below*/}
-            <form action={`/product/${this.props.product._id}?_method=PUT`} method="POST">    
+            <form action={`/products/${this.props.product._id}?_method=PUT`} method="POST">    
             Name: <input type="text" name="name" defaultValue={this.props.product.name}/><br/>
             Description: <input type="text" name="description" defaultValue={this.props.product.description} /><br/>
             Image URL: <input type="text" name="img" defaultValue={this.props.product.img} /><br/>
@@ -37,7 +32,7 @@ class Edit extends React.Component{
         </nav>
         </div>
         </body>
-      </html>
+     
       )
       
     }
